850ba581c330166aed83b02146bae6c54eb11c14
seoyerin0715 <seoyerin0715@gmail.com>
2026-03-25 17:40:46 +0900

    2026. 03. 25

--- list.py
+++ list.py
@@ -6,5 +6,45 @@ for i in addressbook:
     print(i)
 
 '''
+'''
 ri=list(range(5))
 print (ri)
+'''
+'''
+myString= 'apple'
+listString=list(myString)
+for ch in myString:
+    print(ch)
+
+print(listString)
+'''
+'''
+n_list=[10,20,30,40]
+n_list.append(50)
+n_list.remove(10)
+print(n_list)
+'''
+'''
+n_list=[11,22,33,44,55,66]
+a=n_list.pop()
+print(a)
+print(n_list)
+'''
+'''
+a_list=[10,20,30,40]
+print(10 in a_list)
+print(50 in a_list)
+print(10 not in a_list)
+print(50 not in a_list)
+'''
+'''
+list1=[20,10,40,50,30]
+print(list1)
+list2= sorted(list1)
+print(list2)
+'''
+
+list1=[1,2,3,4]
+list2=[2,3,3,1]
+print(list1>list2)
+print(list1<list2)
