55d13b99d9815659625422c00da1d69b42e37bd8
seoyerin0715 <seoyerin0715@gmail.com>
2026-03-25 16:44:19 +0900

    2026. 03. 25

---
+++ list.py
@@ -0,0 +1,10 @@
+'''
+score= [87,84,95,67,88,94,63]
+names=["영호","순자","영식","순희",score]
+addressbook=[["영호",25,"010-3185-8412"],["나나난ㄴ",25,"010-3185-8412"]]
+for i in addressbook:
+    print(i)
+
+'''
+ri=list(range(5))
+print (ri)
